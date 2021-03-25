7b5c4c9ea7003708148f7277d51a3620b088d816
MSerperior <danychristian55@gmail.com>
2021-03-25 21:04:21 +0700

    add empty methods to be implemented

--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,22 @@
 
 public class Stack {
-
+	
+	public void push() {
+		
+	}
+	public void pop() {
+		
+	}
+	public void peek() {
+		
+	}
+	public void clear() {
+		
+	}
+	public void isFull() {
+		
+	}
+	public void isEmpty() {
+		
+	}
 }
