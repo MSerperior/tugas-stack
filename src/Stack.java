c41ec2df035c48dbdf1c853fc1c0affa959db43b
MSerperior <danychristian55@gmail.com>
2021-03-29 12:22:52 +0700

    testing stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,6 +1,15 @@
 
 public class Stack {
 	
+	int stack[] = new int[10];
+	
+	public void fillTestData() {
+		stack[0] = 3;
+		stack[1] = 6;
+		stack[2] = 5;
+		stack[3] = 2;
+		stack[4] = 7;
+	}
 	public void push() {
 		
 	}
